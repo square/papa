a1819e4034dbf20b870f25a672b8e7fc5bd51b3c
Jake Staahl <jstaahl@snapchat.com>
2022-06-10 15:06:52 -0700

    Use MessageCompat.setAsynchronous to support API 16+

--- papa/src/main/java/papa/internal/Handlers.kt
+++ papa/src/main/java/papa/internal/Handlers.kt
@@ -1,17 +1,15 @@
 package papa.internal
 
-import android.os.Build
 import android.os.Handler
 import android.os.Looper
 import android.os.Message
+import androidx.core.os.MessageCompat
 
 // Thx @chet and @jreck
 // https://cs.android.com/androidx/platform/frameworks/support/+/androidx-main:metrics/metrics-performance/src/main/java/androidx/metrics/performance/JankStatsApi16Impl.kt;l=66;drc=523d7a11e46390281ed3f77893671730cd6edb98
 internal fun Handler.postAtFrontOfQueueAsync(callback: () -> Unit) {
   sendMessageAtFrontOfQueue(Message.obtain(this, callback).apply {
-    if (Build.VERSION.SDK_INT >= 22) {
-      isAsynchronous = true
-    }
+    MessageCompat.setAsynchronous(this, true)
   })
 }
 
